fcec88551ab0e23c917df66eb4c4406aaa73acdc
Centurion195 <Vadim-0211@yandex.ru>
2023-02-08 15:35:37 +0300

    Task #2 completed

---
+++ #2/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задать массив, заполнить случайными положительными трёхзначными числами. Показать количество нечетных\четных чисел
+
+int[] CreateArray(int length, int lowest, int hightest){
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(lowest,hightest+1);
+    return array;
+}
+
+void PrintArray(int[] array){
+    foreach (int element in array)
+        Console.Write($"| {element} ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int FindAnEvenNumber(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i]%2==0) count++;
+    return count;
+}
+
+int length = ReadInt("Введите длину массива: ");;
+int lowest = 100;
+int hightest = 999;
+int[] array = CreateArray(length, lowest, hightest);
+
+PrintArray(array);
+Console.WriteLine();
+Console.WriteLine($"Количество четных чисел в массиве: {FindAnEvenNumber(array)}");
+Console.WriteLine($"Количество нечетных чисел в массиве: {array.Length-FindAnEvenNumber(array)}");
