ebbd647d9c79ca336f13c42b316b726ff85a4310
Centurion195 <Vadim-0211@yandex.ru>
2023-02-08 15:02:22 +0300

    Task #1 completed

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+
+byte[] CreateArray(int length, int lowest, int hightest){
+    byte[] array = new byte[length];
+    Random random = new Random();
+    for (int i = 0; i < array.Length; i++)
+        array[i] = Convert.ToByte(random.Next(lowest,hightest+1));
+    return array;
+}
+
+void PrintArray(byte[] array){
+    foreach (byte element in array)
+        Console.Write($"| {element} ");
+}
+int length = 8;
+int lowest = 0;
+int hightest = 1;
+
+PrintArray(CreateArray(length, lowest, hightest));
